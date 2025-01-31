6ad82889baac517046988d0d6ea4c71a002e4547
leejunggyun <lee09260@naver.com>
2025-01-31 18:13:58 +0900

    st7735 function revision 1. cmd function add 2. data send function add

--- st7735.c
+++ st7735.c
@@ -2,163 +2,107 @@
 #include "gpio.h"
 #include "spi.h"
 
-static void st7735_sleep_out()
+static void st7735_send_cmd(uint8_t cmd)
 {
-    uint8_t tx_buffer = 0;
-    tx_buffer = 0x11;
     GPIO_A0_LOW;
     GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer, sizeof(tx_buffer));
+    spi_half_duplex_write(SPIDEV0_0, &cmd, sizeof(cmd));
     GPIO_CS_HIGH;
     GPIO_A0_LOW;
+}
+
+static void st7735_send_data(uint8_t cmd, uint8_t *buffer, size_t buffer_size, uint32_t repeat)
+{
+    uint32_t i = 0;
+    GPIO_A0_LOW;
+    GPIO_CS_LOW;
+    spi_half_duplex_write(SPIDEV0_0, &cmd, sizeof(cmd));
+    GPIO_A0_HIGH;
+    for (i = 0; i < repeat; i++)
+    {
+        spi_half_duplex_write(SPIDEV0_0, buffer, buffer_size);
+    }
+    GPIO_CS_HIGH;
+    GPIO_A0_LOW;
+}
+
+static void st7735_sleep_out()
+{
+    st7735_send_cmd(0x11);
 }
 
 static void st7735_display_on()
 {
-    uint8_t tx_buffer = 0;
-    tx_buffer = 0x29;
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer, sizeof(tx_buffer));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    st7735_send_cmd(0x29);
 }
 
 static void st7735_display_off()
 {
-    uint8_t tx_buffer = 0;
-    tx_buffer = 0x28;
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer, sizeof(tx_buffer));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    st7735_send_cmd(0x28);
 }
 
 static void st7735_set_idle_on()
 {
-    uint8_t tx_buffer = 0;
-    tx_buffer = 0x39;
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer, sizeof(tx_buffer));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    st7735_send_cmd(0x39);
 }
 
 static void st7735_set_idle_off()
 {
-    uint8_t tx_buffer = 0;
-    tx_buffer = 0x38;
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer, sizeof(tx_buffer));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    st7735_send_cmd(0x38);
 }
 
 static void st7735_color_mode_set_18bit()
 {
-    uint8_t tx_buffer1 = 0;
-    uint8_t tx_buffer2 = 0;
+    uint8_t tx_buffer = 0;
 
-    tx_buffer1 = 0x3a;
-    tx_buffer2 = 0x06;
-
-    printf("send msg 0x%02x 0x%02x\n", tx_buffer1, tx_buffer2);
-
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer1, sizeof(tx_buffer1));
-    GPIO_A0_HIGH;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer2, sizeof(tx_buffer2));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    tx_buffer = 0x06;
+    st7735_send_data(0x3a, &tx_buffer, sizeof(tx_buffer), 1);
 }
 
 static void st7735_ram_address_set()
 {
-    uint8_t tx_buffer1 = 0;
-    uint8_t tx_buffer2 = 0;
+    uint8_t tx_buffer = 0;
 
-    tx_buffer1 = 0x36;
-    tx_buffer2 = 0x08;
-
-    printf("send msg 0x%02x 0x%02x\n", tx_buffer1, tx_buffer2);
-
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer1, sizeof(tx_buffer1));
-    GPIO_A0_HIGH;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer2, sizeof(tx_buffer2));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    tx_buffer = 0x08;
+    st7735_send_data(0x36, &tx_buffer, sizeof(tx_buffer), 1);
 }
 
 static void st7735_column_set(uint16_t x_start, uint16_t x_end)
 {
-    uint8_t tx_buffer1 = 0;
-    uint8_t tx_buffer2[4] = {
+    uint8_t tx_buffer[4] = {
         0,
     };
-    tx_buffer1 = 0x2a;
-    tx_buffer2[0] = (x_start >> 8) & 0xff;
-    tx_buffer2[1] = x_start & 0xff;
-    tx_buffer2[2] = (x_end >> 8) & 0xff;
-    tx_buffer2[3] = x_end & 0xff;
 
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer1, sizeof(tx_buffer1));
-    GPIO_A0_HIGH;
-    spi_half_duplex_write(SPIDEV0_0, tx_buffer2, sizeof(tx_buffer2));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    tx_buffer[0] = (x_start >> 8) & 0xff;
+    tx_buffer[1] = x_start & 0xff;
+    tx_buffer[2] = (x_end >> 8) & 0xff;
+    tx_buffer[3] = x_end & 0xff;
+    st7735_send_data(0x2a, tx_buffer, sizeof(tx_buffer), 1);
 }
 
 static void st7735_raw_set(uint16_t y_start, uint16_t y_end)
 {
-    uint8_t tx_buffer1 = 0;
-    uint8_t tx_buffer2[4] = {
+    uint8_t tx_buffer[4] = {
         0,
     };
-    tx_buffer1 = 0x2b;
-    tx_buffer2[0] = (y_start >> 8) & 0xff;
-    tx_buffer2[1] = y_start & 0xff;
-    tx_buffer2[2] = (y_end >> 8) & 0xff;
-    tx_buffer2[3] = y_end & 0xff;
 
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer1, sizeof(tx_buffer1));
-    GPIO_A0_HIGH;
-    spi_half_duplex_write(SPIDEV0_0, tx_buffer2, sizeof(tx_buffer2));
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    tx_buffer[0] = (y_start >> 8) & 0xff;
+    tx_buffer[1] = y_start & 0xff;
+    tx_buffer[2] = (y_end >> 8) & 0xff;
+    tx_buffer[3] = y_end & 0xff;
+    st7735_send_data(0x2b, tx_buffer, sizeof(tx_buffer), 1);
 }
 
 static void st7735_write_color(uint8_t red, uint8_t green, uint8_t blue, uint16_t x_len, uint16_t y_len)
 {
-    uint8_t tx_buffer1 = 0;
-    uint8_t tx_buffer2[3] = {
+    uint8_t tx_buffer[3] = {
         0,
     };
-    uint32_t i = 0;
-    tx_buffer1 = 0x2c;
-    tx_buffer2[0] = red;
-    tx_buffer2[1] = blue;
-    tx_buffer2[2] = green;
 
-    GPIO_A0_LOW;
-    GPIO_CS_LOW;
-    spi_half_duplex_write(SPIDEV0_0, &tx_buffer1, sizeof(tx_buffer1));
-    GPIO_A0_HIGH;
-    for (i = 0; i < x_len * y_len; i++)
-    {
-        spi_half_duplex_write(SPIDEV0_0, tx_buffer2, sizeof(tx_buffer2));
-    }
-    GPIO_CS_HIGH;
-    GPIO_A0_LOW;
+    tx_buffer[0] = red;
+    tx_buffer[1] = blue;
+    tx_buffer[2] = green;
+    st7735_send_data(0x2c, tx_buffer, sizeof(tx_buffer), x_len * y_len);
 }
 
 static void st7735_init()
